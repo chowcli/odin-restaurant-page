196b05bb108e724e4515fbe1f26f799ff9a11b12
chowcli <136085102+chowcli@users.noreply.github.com>
2023-09-17 22:00:52 +0700

    Finish styling for menu and contact content

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,7 @@ import "../styles/index.css";
 import "../styles/header.css";
 import "../styles/footer.css";
 import "../styles/home.css";
+import "../styles/menu.css";
+import "../styles/contact.css";
 
 document.addEventListener("DOMContentLoaded", () => initializeWebsite());
