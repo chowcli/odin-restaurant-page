69e2fa595fa236cc200c6c23a59e5dca182d50bb
chowcli <136085102+chowcli@users.noreply.github.com>
2023-09-18 10:23:31 +0700

    Initial dist subtree commit

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import initializeWebsite from "./app";
-import "../styles/index.css";
-import "../styles/header.css";
-import "../styles/footer.css";
-import "../styles/home.css";
-import "../styles/menu.css";
-import "../styles/contact.css";
-
-document.addEventListener("DOMContentLoaded", () => initializeWebsite());
